23d5a89806f455305490526c2f506ff9e37e39a7
TKH <tallonkh@gmail.com>
2018-05-11 18:35:25 -0400

    + Added some (optional) parsing statistics. + parse() now returns the JSON output. + Added way to stop parser forcefully ~ If only one language is selected, parser will not bother specifying each word's language in JSON output. ~ Plurals for English nouns now work better. - Stopped pretending that plurals would work for other languages. They don't. (for now). - Made program more user-proof. - Solving plurals is now forced because unsolved plurals are terrible.

--- wiktionaryparse.py
+++ wiktionaryparse.py
@@ -11,27 +11,82 @@ title34PT = re.compile('====?([^=]+)====?')
 plrre = re.compile('{{(\w{2,3}-(?:noun|verb).*)}}')
 # matches default plural rule (append s to word)
 plrBase = re.compile('\w{2,3}-(?:noun|verb)')
-
+# levels of countability corresponding with indeces 0,1,2
+countableTypes = ['No', 'Yes', 'Sometimes']
 # split a string into tags {{a|b|c}}
 def tagify(s):
 	return s[2:-2].split('|')
 
 class WiktionaryParser():
 	def __init__(self, inpath, outpath):
-		self.inpath = inpath	# input XML file's path
-		self.outpath = outpath	# output JSON file's path
-		self.maxPageCount = 25	# maximum number of words/pages to save
-		# words in these languages should be saved
-		self.targetLangs = {'English'}
+		self.__inpath = inpath	# input XML file's path
+		self.__outpath = outpath	# output JSON file's path
+		self.__maxPageCount = 100	# maximum number of words/pages to save
+		# words in these languages should be saved - set empty or None to accept all languages
+		self.__targetLangs = {'English'}
+		self.__oneLang = len(self.__targetLangs) == 1
 		# content in these sections should be saved
-		self.targetSections = {'Noun', 'Verb', 'Adjective'}
+		self.__targetSections = {'Noun', 'Verb', 'Adjective'}
 		# if info about plurals should be tracked
-		self.trackPlurals = True
-		# if the plurals themselves should be tracked - requires trackPlurals == True
-		self.solvePlurals = True
+		self.__trackPlurals = True
+		# if the parser is currently running
+		self.__running = False
+		# if the parser should track info about how much data it has parsed
+		self.__trackSelf = True
+
+	def setInPath(path):
+		if(self.__running):
+			print('Cannot change settings while running!')
+			return None
+		self.__inpath = path
+		return True
+
+	def setOutPath(path):
+		if(self.__running):
+			print('Cannot change settings while running!')
+			return None
+		self.__outpath = path
+		return True
+
+	def setMaxPageCount(count):
+		if(self.__running):
+			print('Cannot change settings while running!')
+			return None
+		self.__maxPageCount = count
+		return True
+
+	def setTargetLanguages(*langs):
+		if(self.__running):
+			print('Cannot change settings while running!')
+			return None
+		self.__targetLangs = set(langs)
+		self.__oneLang = len(langs) == 1
+		return True
+
+	def setTrackPlurals(track):
+		if(self.__running):
+			print('Cannot change settings while running!')
+			return None
+		self.__trackPlurals = track
+		return True
+
+	# stops the parser before its next iteration
+	def forceStop():
+		if(self.__running):
+			self.__running = False
+			return True
+		else:
+			print('Can\'t stop if not running!')
+			return False
 
 	def parse(self):
-		inf = open(self.inpath, 'r')
+		# prevent stupidity
+		if(self.__running):
+			print('Failed to run. This parser is already running!')
+			return None
+		self.__running = True
+
+		inf = open(self.__inpath, 'r')
 
 		pageCount = 0	# number of saved pages
 		pages = dict()	# holds all pages - one page per word
@@ -42,6 +97,7 @@ class WiktionaryParser():
 
 		# HEIRARCHY: word/page > language > section > contents
 		currentLang = None	# dict of a language's sections
+		currentLangName = None	# name of the current language
 		currentSection = None	# dict of a section's contents
 		currentSectionName = None	# name of the current section
 
@@ -55,8 +111,18 @@ class WiktionaryParser():
 		# bool - the whole section should be skipped
 		skipSection = False
 
-		while pageCount < self.maxPageCount:
+		linesRead = 0
+		bytesRead = 0
+
+		while (pageCount < self.__maxPageCount):
+			if(not self.__running):
+				print('Stopping forcefully...')
+				break
 			line = inf.readline()
+
+			if(self.__trackSelf):
+				linesRead += 1
+				bytesRead += len(line.decode('utf8'))
 
 			# if line is empty, end of file has been reached
 			if(not line):
@@ -80,6 +146,8 @@ class WiktionaryParser():
 						# reset some variables
 						hasContent = False
 						currentLang = None
+						currentLangName = None
+						currentName = None
 						currentSection = None
 						currentSectionName = None
 						foundPlurals = False
@@ -124,6 +192,7 @@ class WiktionaryParser():
 				# if lang ended
 				if(line == '----'):
 					currentLang = None
+					currentLangName = None
 					continue
 
 				# skip the whole language
@@ -131,21 +200,28 @@ class WiktionaryParser():
 					continue
 
 				# if not currently in a language section, check to see if a new one started
-				# compare None because 'not currentLang' would match empty sets too
-				if(currentLang == None):
+				if(currentLangName == None):
 					# check if this is a language title
 					match = title2PT.match(line)
 					if(match):
 						# if it is a language title, extract the language
 						lang = match.group(1)
 						# check if this language is desired
-						if((not self.targetLangs) or (lang in self.targetLangs)):
-							# create a dict for this language's sections
-							currentLang = dict()
-							# add the language section to the word's page
-							page[lang] = currentLang
+						if((not self.__targetLangs) or (lang in self.__targetLangs)):
+							currentLangName = lang
+							# if there's only one language, skip creating a -
+							# lang section and enter info right into the page dict.
+							# This is a really sketchy way to do this, but it's efficient and it works.
+							if(self.__oneLang):
+								currentLang = page
+							else: # otherwise, if there are multiple langs:
+								# create a dict for this language's sections
+								currentLang = dict()
+								# add the language section to the word's page
+								page[lang] = currentLang
 						else:
 							currentLang = None
+							currentLangName = None
 							# if the language isn't desired, skip it
 							skipLang = True
 					# if not in a lang, don't bother checking for sections
@@ -157,7 +233,7 @@ class WiktionaryParser():
 					# if it is a section title, extract the section type
 					section = match.group(1)
 					# check if this section is desired
-					if((not self.targetSections) or (section in self.targetSections)):
+					if((not self.__targetSections) or (section in self.__targetSections)):
 						currentSectionName = section
 						# create a dict for this section's contents
 						currentSection = dict()
@@ -177,51 +253,80 @@ class WiktionaryParser():
 				# if currently in a specific section...
 				if(currentSectionName == 'Noun'):
 					# check for plural rules if necessary
-					if(self.trackPlurals and not foundPlurals):
+					# currently locked to English only... other languages have different plural rules.
+					if(self.__trackPlurals and not foundPlurals and currentLangName == 'English'):
 						# try to match the plural rules line
 						match = plrre.match(line)
 						if(match):
 							# if plural rules line is found, extract the tags
 							plurtags = tagify(match.group(0))
+							numPlurTags = len(plurtags)
 
-							# if specified, replace rules with the pluralized word
-							if(self.solvePlurals):
-								for i in range(len(plurtags)-1, -1, -1):
+							# converts plural notation to actual plural words
+							solvedPlurs = []
+
+							# is word countable?
+							# 0: no, 1: yes, 2: maybe
+							countable = 0
+							 # default rule en-noun - add an s
+							if(numPlurTags == 1):
+								countable = 1
+								solvedPlurs.append(currentWord + 's')
+							else:
+								# remove the ISO-noun tag
+								plurtags.pop(0)
+								numPlurTags -= 1
+								for i in range(numPlurTags):
 									rule = plurtags[i]
-									if(rule == 's' or plrBase.match(rule)):
-										# default rule ISO-noun - add an s
-										plurtags[i] = currentWord + 's'
+									if(rule == 's'):
+										solvedPlurs.append(currentWord + 's')
+										# do not change if countable = sometimes
+										if(countable == 0):
+											countable = 1
 									elif(rule == 'es'):
-										# add an 'es'
-										plurtags[i] = currentWord + 'es'
-									elif(rule == '~'):
-										# TODO figure out how to handle this...
-										plurtags.pop(i)
-									elif(rule == '-'):
-										# non-countable
-										plurtags = None
-										break
+										solvedPlurs.append(currentWord + 'es')
+										# do not change if countable = sometimes
+										if(countable == 0):
+											countable = 1
+									elif(rule == '~'): # sometimes countable
+										countable = 2
+										# if ~ is the only tag, assume plural is +s
+										if(numPlurTags == 1):
+											solvedPlurs.append(currentWord + 's')
+									elif(rule == '-'): # non-countable or rarely countable
+										if(countable == 1):
+											countable = 2
+									else:
+										# do not change if countable = sometimes
+										if(countable == 0):
+											countable = 1
+										solvedPlurs.append(rule)
 
-							# plurtags will be falsy if a - causes it to be None
-							if(plurtags):
-								currentSection['plural'] = plurtags
+							currentSection['countable'] = countableTypes[countable]
+
+							# solvedPlurs will be falsy if a - causes it to be None
+							if(solvedPlurs):
+								currentSection['plural'] = solvedPlurs
 							continue
-						else:
-							# it's not a plural rules line...
-							pass
 				elif(currentSectionName == 'Verb'):
 					pass
 				elif(currentSectionName == 'Adjective'):
 					pass
 		inf.close()
 
-		# for word in pages:
-		# 	print(word)
-		print('Pages:' + str(len(pages)))
-
-		outf = open(self.outpath, 'w')
-		outf.write(json.dumps(pages))
+		js = json.dumps(pages)
+		outf = open(self.__outpath, 'w')
+		outf.write(js)
 		outf.close()
+
+		print('Finished with ' + str(len(pages)) + ' words.')
+		if(self.__trackSelf):
+			print('Parsed ' + str(linesRead) + ' pages.')
+			print('Turned ' + str(bytesRead) + ' bytes into ' + str(len(js.decode('utf8'))) + '.')
+
+		return js
+
+#TODO maybe check the API to update existing words in the JSON
 
 def example():
 	parser = WiktionaryParser('/Users/default/Documents/Wikiparse/wiktionary.xml', '/Users/default/Documents/Wikiparse/parsed.json')
